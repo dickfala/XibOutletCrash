9cfafd95a4a2573d237b9ecdf1363201c761a7a5
james <dickfala@gmail.com>
2015-05-05 22:51:38 +0800

    Initial Commit

---
+++ xibDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xibDemo
+//
+//  Created by Chang YuanYu on 2015/5/5.
+//  Copyright (c) 2015年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xibDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xibDemo
+//
+//  Created by Chang YuanYu on 2015/5/5.
+//  Copyright (c) 2015年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
